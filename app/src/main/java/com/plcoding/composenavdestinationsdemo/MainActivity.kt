4b1458e2c501d3ba5d72a30837830832d948fbf6
Paul <jpdcands211@gmail.com>
2022-09-02 14:15:53 -0400

    1st Step Navigation

--- app/src/main/java/com/plcoding/composenavdestinationsdemo/MainActivity.kt
+++ app/src/main/java/com/plcoding/composenavdestinationsdemo/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.plcoding.composenavdestinationsdemo
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -15,8 +15,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -24,8 +31,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.plcoding.composenavdestinationsdemo.NavGraphs.root
-import com.plcoding.composenavdestinationsdemo.destinations.PostScreenDestination
-import com.plcoding.composenavdestinationsdemo.destinations.ProfileScreenDestination
+import com.plcoding.composenavdestinationsdemo.destinations.LoginScreenDestination
+//import com.plcoding.composenavdestinationsdemo.destinations.ScreenThreeDestination
+import com.plcoding.composenavdestinationsdemo.destinations.ScreenOneDestination
+import com.plcoding.composenavdestinationsdemo.destinations.ScreenTwoDestination
 import com.plcoding.composenavdestinationsdemo.ui.theme.ComposeNavDestinationsDemoTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
@@ -54,54 +63,80 @@ fun LoginScreen(
 ) {
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Login Screen")
+        Text(
+       "Login Screen",
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            fontSize = 50.sp,
+            modifier = Modifier
+                  .padding(top = 160.dp, bottom = 140.dp)
+                  .border(4.dp, Color.Black, RectangleShape)
+                  .padding(15.dp)
+        )
         Button(onClick = {
             navigator.navigate(
-                ProfileScreenDestination(
-                    User(
-                        name = "Chris P. Bacon",
-                        id = "userid",
-                        created = LocalDateTime.now()
-                    )
-                )
-            )
+                ScreenOneDestination())
         }) {
-            Text("Go to Profile Screen")
+            Text("Go to Screen 1")
         }
     }
 }
 
 @Destination
 @Composable
-fun ProfileScreen(
+fun ScreenOne(
     navigator: DestinationsNavigator,
-    user: User
 ) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Profile Screen: $user", textAlign = TextAlign.Center)
+       Text("Screen 1",
+           color = Color.Blue,
+           fontWeight = FontWeight.Bold,
+           textAlign = TextAlign.Center,
+           fontSize = 50.sp,
+           modifier = Modifier
+               .padding(top = 100.dp, bottom = 140.dp)
+               .border(4.dp, Color.Black, RectangleShape)
+               .padding(15.dp)
+       )
         Button(onClick = {
-            navigator.navigate(PostScreenDestination())
+            navigator.navigate(ScreenTwoDestination())
         }) {
-            Text("Go to Post Screen")
+            Text("Go to Screen 2")
         }
     }
 }
 @Destination
 @Composable
-fun PostScreen(
-    showOnlyPostsByUser: Boolean = false
+fun ScreenTwo(
+    navigator: DestinationsNavigator
 ) {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Post Screen, $showOnlyPostsByUser")
+        Text("Screen 2",
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            fontSize = 50.sp,
+            modifier = Modifier
+                .padding(top = 160.dp, bottom = 140.dp)
+                .border(4.dp, Color.Black, RectangleShape)
+                .padding(15.dp)
+        )
+        Button(onClick = {
+            navigator.navigate(LoginScreenDestination())
+        }) {
+            Text("Go to  LogIn Screen")
+        }
     }
 }
